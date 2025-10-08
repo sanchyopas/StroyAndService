8a97746b6bb3ff623ed764b273030f3aa1318144
avroratomsk <avroratomsk70@yande.ru>
2025-10-08 14:23:05 +0300

    dev: hero block

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
 import * as functions from './modules/baseFunctions.js';
+import Swiper from "swiper";
 
 functions.isWebp();
+
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
