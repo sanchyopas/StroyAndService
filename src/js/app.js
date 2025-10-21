1a0dd72022c369ed2c1250fc84e309559e4558e6
avroratomsk <avroratomsk70@yandex.ru>
2025-10-21 11:49:38 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,83 +1,94 @@
 import * as functions from './modules/baseFunctions.js';
 import Swiper from "swiper";
-import {Navigation, Pagination, Scrollbar } from "swiper/modules";
+import {Navigation, Pagination, Scrollbar} from "swiper/modules";
 
 functions.isWebp();
 
 import "./modules/tabs.js"
 
 const heroSlider = new Swiper('.hero__slider', {
-  modules: [Navigation, Scrollbar],
-  direction: 'horizontal',
-  slidesPerView: 2.5,
-  spaceBetween: 20,
+    modules: [Navigation, Scrollbar],
+    direction: 'horizontal',
+    slidesPerView: 2.5,
+    spaceBetween: 20,
 
-  navigation: {
-    nextEl: '.hero__btn-next',
-    prevEl: '.hero__btn-prev',
-  },
+    navigation: {
+        nextEl: '.hero__btn-next',
+        prevEl: '.hero__btn-prev',
+    },
 
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
 
 const aboutSlider = new Swiper('.about__slider', {
-  modules: [Scrollbar, Navigation],
-  direction: 'horizontal',
-  slidesPerView: 1,
-  spaceBetween: 20,
+    modules: [Scrollbar, Navigation],
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 20,
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-  navigation: {
-    nextEl: '.about__btn-next',
-    prevEl: '.about__btn-prev',
-  },
+    navigation: {
+        nextEl: '.about__btn-next',
+        prevEl: '.about__btn-prev',
+    },
 
-  scrollbar: {
-    el: '.about__scrollbar',
-  },
+    scrollbar: {
+        el: '.about__scrollbar',
+    },
 });
 
 const serviceSlider = new Swiper('.service__slider', {
-  modules: [Scrollbar, Navigation],
-  direction: 'horizontal',
-  slidesPerView: 4,
-  spaceBetween: 37,
+    modules: [Scrollbar, Navigation],
+    direction: 'horizontal',
+    spaceBetween: 37,
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-  navigation: {
-    nextEl: '.about__btn-next',
-    prevEl: '.about__btn-prev',
-  },
+    navigation: {
+        nextEl: '.about__btn-next',
+        prevEl: '.about__btn-prev',
+    },
 
-  scrollbar: {
-    el: '.about__scrollbar',
-  },
+    scrollbar: {
+        el: '.about__scrollbar',
+    },
+
+    breakpoints: {
+        1200: {
+            slidesPerView: 4,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 2,
+        }
+    }
 });
 
 const videoSlider = new Swiper('.video__slider', {
-  modules: [Scrollbar, Navigation],
-  direction: 'horizontal',
-  slidesPerView: 4,
-  spaceBetween: 37,
+    modules: [Scrollbar, Navigation],
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 37,
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-  navigation: {
-    nextEl: '.about__btn-next',
-    prevEl: '.about__btn-prev',
-  },
+    navigation: {
+        nextEl: '.about__btn-next',
+        prevEl: '.about__btn-prev',
+    },
 
-  scrollbar: {
-    el: '.about__scrollbar',
-  },
+    scrollbar: {
+        el: '.about__scrollbar',
+    },
 });
